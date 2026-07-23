3b9b6c916fa78960413d65f1c81a41d574e12104
872056224 <872056224@qq.com>
2026-07-23 10:45:30 +0800

    fix: insert default attendance for all missing workdays (June-July)

--- docs/sql/2026-07-23-fill-missing-attendance.sql
+++ docs/sql/2026-07-23-fill-missing-attendance.sql
@@ -1,19 +1,33 @@
--- 对工作日且 ≤ 今天的 attendance 记录中，check_in/check_out 都为空（完全没打卡）的
--- 填充 09:00-18:00 签到签退，位置马尾区上坂路
+-- 为 6-7 月所有工作日且无 attendance 记录的员工补上缺省记录
+-- 签到 09:00 / 签退 18:00 / 位置马尾区上坂路
 
 SET @address = '福建省福州市马尾区上坂路';
 
+-- Step 1: 已有记录但签到签退都为空 → 更新
 UPDATE day.attendance a
 JOIN day.holiday h ON a.date = h.date AND h.type = 'WORKDAY'
 SET
-  a.check_in_time = CONCAT(a.date, ' 09:00:00'),
-  a.check_out_time = CONCAT(a.date, ' 18:00:00'),
+  a.check_in_time = CAST(CONCAT(DATE_FORMAT(a.date, '%Y-%m-%d'), ' 09:00:00') AS DATETIME),
+  a.check_out_time = CAST(CONCAT(DATE_FORMAT(a.date, '%Y-%m-%d'), ' 18:00:00') AS DATETIME),
   a.check_in_address = @address,
   a.check_out_address = @address,
   a.today_status = 'CHECKED_OUT'
 WHERE
-  a.date <= CURDATE()
+  a.date BETWEEN '2026-06-01' AND CURDATE()
   AND a.check_in_time IS NULL
   AND a.check_out_time IS NULL;
+SELECT ROW_COUNT() AS 'updated';
 
-SELECT ROW_COUNT() AS 'affected_rows';
+-- Step 2: 完全无记录的工作日 → 为每个员工插入一条
+INSERT IGNORE INTO day.attendance(emp_id, date, check_in_time, check_out_time,
+  check_in_address, check_out_address, today_status)
+SELECT e.number, h.date,
+  CAST(CONCAT(DATE_FORMAT(h.date, '%Y-%m-%d'), ' 09:00:00') AS DATETIME),
+  CAST(CONCAT(DATE_FORMAT(h.date, '%Y-%m-%d'), ' 18:00:00') AS DATETIME),
+  @address, @address, 'CHECKED_OUT'
+FROM day.holiday h
+CROSS JOIN day.emp e
+WHERE h.date BETWEEN '2026-06-01' AND CURDATE()
+  AND h.type = 'WORKDAY'
+  AND NOT EXISTS (SELECT 1 FROM day.attendance a WHERE a.date = h.date AND a.emp_id = e.number);
+SELECT ROW_COUNT() AS 'inserted';
