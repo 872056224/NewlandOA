3b3b38b4d3497a392e11b3b6eeb2cc68b432c30c
872056224 <872056224@qq.com>
2026-07-23 19:00:04 +0800

    chore: 屏蔽文档和md文件，只保留STARTUP.md

--- docs/sql/2026-07-21-add-leave-type.sql
+++
@@ -1,8 +0,0 @@
--- OA 系统 Phase 1: 请假管理 - 数据库迁移脚本
--- 执行方式: mysql -u root -p day < 2026-07-21-add-leave-type.sql
-
-USE `day`;
-
-ALTER TABLE `leave`
-  ADD COLUMN `type` varchar(20) NOT NULL DEFAULT '事假' COMMENT '请假类型: 事假/病假/年假/调休'
-  AFTER `name`;
